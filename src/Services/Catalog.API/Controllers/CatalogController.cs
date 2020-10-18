0437f1e255b516b9ef389a894314e2cc0f1b513c
Parag Patil <parag.patil@live.com>
2020-10-17 17:20:04 -0700

    Added Catalog Controller and Product Entity Class

---
+++ src/Services/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+    }
+}
